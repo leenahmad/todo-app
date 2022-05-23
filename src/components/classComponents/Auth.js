ac5a9680d2f5a02792f5f1c111fcea91d6a1b41c
leenahmad <leenkaraja24@gmail.com>
2022-05-23 21:31:43 +0300

    lab 33

---
+++ src/components/classComponents/Auth.js
@@ -0,0 +1,15 @@
+import {LoginContext} from '../context/login';
+import {When} from 'react-if';
+import {useContext} from 'react';
+
+export default function Auth(props){
+
+    const protect = useContext(LoginContext);
+
+    return( 
+        <When condition={protect.login && protect.authorize(props.action)}>
+            {props.children}
+        </When>
+
+    )
+}
